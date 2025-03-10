59fab3d6eb95c75ef789255c06fcc74b76a413fc
Adar <adar.rasheed@gmail.com>
2025-03-10 17:48:27 +0500

    initial project setup with java 21, maven, web and devtools

---
+++ src/main/java/com/exercise/exchangepay/ExchangepayApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.exchangepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangepayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/exchangepay/ExchangepayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.exchangepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangepayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
